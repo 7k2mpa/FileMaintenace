899d48e4bb4d93eeb8fdae87f64f0e977d33b0db
M.SUDO a.k.a. 7K2MPA a.k.a. doctor_d <THINKPAD+doctor_d@thinkpad.highspec.org>
2020-02-11 23:20:49 +0900

    add license type

--- CommonFunctions.ps1
+++ CommonFunctions.ps1
@@ -1,5 +1,27 @@
 #Requires -Version 5.0
 
+
+<#
+
+.NOTES
+
+Copyright 2020 Masayuki Sudo
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+#>
+
 $Script:CommonFunctionsVersion = '20200130_1050'
 
 #ƒƒO“™‚Ì•Ï”‚ğˆêŠ‡İ’è‚µ‚½‚¢ê‡‚ÍˆÈ‰º‚ğ—˜—p‚µ‚Ä‰º‚³‚¢B
