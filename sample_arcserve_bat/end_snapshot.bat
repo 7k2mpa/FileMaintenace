597585c53cb2160d33fce3def7e5590c40d66a6f
M.SUDO a.k.a. 7K2MPA a.k.a. doctor_d <THINKPAD+doctor_d@thinkpad.highspec.org>
2020-02-27 23:10:32 +0900

    make folder and move files

---
+++ sample_arcserve_bat/end_snapshot.bat
@@ -0,0 +1,24 @@
+@echo off
+
+rem ---- スクリプトディレクトリ ----
+set SC_DIR=%~dp0
+set Myself_name=%~nx0
+
+D:
+
+cd D:\Scripts\Infra
+
+set P_NAME=CheckFlag.ps1
+
+powershell -Noninteractive -Command ".\CheckFlag.ps1 -FlagFolder .\Lock -FlagFile EndSnapshot.flg ; exit $LASTEXITCODE"
+
+	IF not %errorlevel%==0 (
+	call %SC_DIR%MSGPRINT.bat %P_NAME%が異常終了しました。エラーレベル＝%errorlevel% ERROR 100
+	goto :ERR
+	)
+
+exit /B
+
+:ERR
+
+exit /B 100
