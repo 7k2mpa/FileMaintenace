66ec2ab565dd145498d558636a4973f2fec08910
M.SUDO a.k.a. 7K2MPA a.k.a. doctor_d <THINKPAD+doctor_d@thinkpad.highspec.org>
2020-03-14 20:16:43 +0900

    fix comment

--- kickme_first.bat
+++ kickme_first.bat
@@ -3,8 +3,8 @@
 rem ========================================================
 rem ソースを登録するので、1回だけ管理者権限で実行すること
 rem 登録後は起動不要
-rem To regist event source , execute by an administrator onece.
-rem After registration , no need to execute this script.
+rem To regist event source, execute with administrator priviledge once.
+rem After registration, no need to execute this script.
 rem ========================================================
 
 eventcreate /L Application /T INFORMATION /SO Infra /ID 1000 /D "regist a new source in event log"
